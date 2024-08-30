3a13c5bae9fb2ffacf0e5f3fc07ef5319ff942fa
Eric <176455136+bonShonjon@users.noreply.github.com>
2024-08-30 19:15:58 +1000

    Add on hover drawing functionality

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const canvas = document.querySelector(".canvas");
-const canvasRes = 16;
+let brushColour = 'canvas__pixel--black'
+canvasCreate(32);
 
-for (i = 0; i < canvasRes; i++) {
-  const canvasRow = document.createElement("div");
-  canvasRow.className = "canvas__row";
-  for (j = 0; j < canvasRes; j++) {
-    const canvasPixel = document.createElement("div");
-    canvasPixel.className = "canvas__pixel";
-    canvasRow.appendChild(canvasPixel);
+function canvasCreate(canvasRes) {
+  for (i = 0; i < canvasRes; i++) {
+    const canvasRow = document.createElement("div");
+    canvasRow.className = "canvas__row";
+    for (j = 0; j < canvasRes; j++) {
+      const canvasPixel = document.createElement("div");
+      canvasPixel.className = "canvas__pixel";
+      canvasPixel.addEventListener('mouseover', colourPixel);
+      canvasRow.appendChild(canvasPixel);
+    }
+    canvas.appendChild(canvasRow)
   }
-  canvas.appendChild(canvasRow)
+}
+
+function colourPixel(e) {
+  if (!e.target.className.includes(brushColour)) {
+    e.target.className = 'canvas__pixel ' + brushColour;
+  }
 }
