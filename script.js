44a6679629b4e72c3468bed32549e58355247866
Eric <176455136+bonShonjon@users.noreply.github.com>
2024-08-31 15:02:04 +1000

    Add slider which adjusts canvas resolution

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const canvas = document.querySelector(".canvas");
-const btnList = document.querySelectorAll(".button")
+const btnList = document.querySelectorAll(".button");
+const slider = document.querySelector("#resolution-range");
+const sliderText = document.querySelector("#resolution-value");
+
+
 let brushColour = "canvas__pixel--black"
 
-canvasCreate(32);
+canvasCreate(slider.value);
+
 btnList.forEach((btn) => btn.addEventListener("click", onBtnPress))
+slider.addEventListener("input", onSliderInput)
+slider.addEventListener("change", onSliderChange)
 
 function canvasCreate(canvasRes) {
   for (i = 0; i < canvasRes; i++) {
@@ -52,4 +59,17 @@ function highlightSelectedBtn(e) {
   const currentSelection = document.querySelector(".button--selected");
   currentSelection.className = "button";
   e.currentTarget.className = "button button--selected";
+}
+
+function onSliderInput(e) {
+  sliderText.textContent = e.target.value;
+}
+
+function onSliderChange(e) {
+  canvasDestroy();
+  canvasCreate(e.target.value);
+}
+
+function canvasDestroy() {
+  canvas.innerHTML = "";
 }
